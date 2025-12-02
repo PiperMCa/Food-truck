1314f43f54e34a7da019f18904a35e08a4439e90
Maddie Effa <meffa2005@gmail.com>
2025-12-01 18:04:17 -0600

    added static routes to the app.js file

--- app.js
+++ app.js
@@ -9,9 +9,13 @@ app.use(express.json())
 app.use(express.static('public'))
 
 //hook up our api routes for the events and the menu
-app.use('api/v1/events', require('./api/v1/events'))
-app.use('api/v1/menu', require('./api/v1/menu'))
+app.use('/api/v1/events', require('./api/v1/events'))
+app.use('/api/v1/menu', require('./api/v1/menu'))
+
+//routes for our html pages
+app.use('/', require('./routes/pages/events'))
+app.use('/', require('./routes/pages/menu'))
 
 //things printed to the console when the server is running
 app.listen(port, () => console.log(`Listening on port: ${port}`))
-app.listen(port, ()=>console.log(`http//:localhost:${port}`))
+//app.listen(port, ()=>console.log(`http://localhost:${port}`))
