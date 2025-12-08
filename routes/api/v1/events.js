d6474a5a1c3dc00af079e28119e905d85d1bdda7
Maddie Effa <meffa2005@gmail.com>
2025-12-08 13:13:57 -0600

    added the get and post shells for the events and menu files

---
+++ routes/api/v1/events.js
@@ -0,0 +1,20 @@
+const router = require('express').Router()//require express router
+
+
+//gets all of the event items
+ router.get('/api/v1/events', (_, response) =>{
+   
+})
+
+//gets the event item with the specified id
+ router.get('/api/v1/events/:id', (_, response) =>{
+   
+})
+
+//adds a new event item to the list
+ router.post('/api/v1/events', (_, response) =>{
+   
+})
+
+//return the routes
+module.exports = router
---
+++ routes/api/v1/menu.js
@@ -0,0 +1,21 @@
+const router = require('express').Router()//require express router
+
+
+//gets all of the menu items
+ router.get('/api/v1/menu', (_, response) =>{
+   
+})
+
+//gets the menu item with the specified id
+ router.get('/api/v1/menu/:id', (_, response) =>{
+   
+})
+
+//adds a new menu item to the list
+ router.post('/api/v1/menu', (_, response) =>{
+   
+})
+
+//return the routes
+module.exports = router
+
